f4b3c092e0a8b5759e2d2cec82338f931f77e432
Andrew Chou <andrewchou@Andrews-MacBook-Pro.local>
2020-04-08 11:14:44 -0700

    basic game loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Decks;
 
 namespace hackathon
@@ -11,6 +12,22 @@ namespace hackathon
             Deck uno = new Deck();
             uno.Shuffle();
             printDeck(uno);
+            // setting up game
+            Console.WriteLine("What's your name player...?");
+            string name = Console.ReadLine();
+            Player player = new Player(name);
+            Player computer = new Player("Computer");
+            for(int i = 0; i < 6; i++){
+                player.Draw(uno);
+                computer.Draw(uno);
+            }
+            // game loop
+            bool playing = true;
+            while(playing){
+                // game logic
+                playing = false;
+            }
+            Console.WriteLine("game over...");
         }
         public static void printDeck(Deck deck){
             foreach (Card card in deck.Cards)
